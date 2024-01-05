1e11480c0de9f7064d3bf63ca04f479d2ef8c3fb
Mustafa Özmen <ozmenmustafa06@gmail.com>
2024-01-05 21:12:41 +0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,26 @@
+for x in range(6):
+    print(x)
+
+for x in range(5,10):
+    print(x)
+
+for x in range(5,30,2):
+    print(x)
+
+for x in range(1,10):
+    print('Erdinç')
+
+for x in range(100,10,-5):
+    print(x)
+
+for x in 'Erdinç':
+    print(x)
+
+for a in range (10): print ("-"*a)
+
+'''for x in 'Mustafa':
+    print(x,end="")'''
+
+a="Mustafa"
+for x in range(len(a)-1,-1,-1):
+    print(a[x],end="")
---
+++ turtle1.py
@@ -0,0 +1,6 @@
+import turtle
+
+for x in range(3):
+    turtle.right(90)
+    turtle.forward(45)
+    turtle.right(30)
