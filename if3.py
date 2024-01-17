74e4e43250fcaa88d47e3cd1aa21eec986055286
ozmenmustafa06 <ozmenmustafa06@gmail.com>
2024-01-17 21:09:55 +0300

    deneme

---
+++ if3.py
@@ -0,0 +1,10 @@
+a = (4>5) or (3<4)
+if a : print("xxxx sonuç true")
+else : print("yyyy sonuç false")
+
+'''
+if (4>5) or (3<4) : print("xxxx sonuç true")
+else : print("yyyy sonuç false")
+'''
+
+# a != b ||| not (a==b) ikisi aynı
