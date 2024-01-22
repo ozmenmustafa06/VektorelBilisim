2064841333941cbaddbc0357136d59808f17ae77
ozmenmustafa06 <ozmenmustafa06@gmail.com>
2024-01-22 19:48:40 +0300

    Deneme

---
+++ fonksiyonlar03.py
@@ -0,0 +1,10 @@
+def topla(*sayi):
+    print(len(sayi))
+    toplam-0
+    for x in sayi:
+        toplam += x
+    return toplam
+    #print(sayi[0])
+    #print(sayi[1])
+
+print("Toplam:", topla(10,20,14))
