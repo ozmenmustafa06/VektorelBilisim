54b9b55455551c19a52e676e8513985da6b70d11
ozmenmustafa06 <ozmenmustafa06@gmail.com>
2024-02-05 00:39:34 +0300

    deneme

---
+++ OOP03.py
@@ -0,0 +1,14 @@
+class calisan:
+    pass
+
+calisan1=calisan()
+
+calisan1.name="Ali"
+calisan1.surname="Veli"
+calisan1.age=20
+print(calisan1.name)
+print(calisan1.surname)
+
+calisan2=calisan()
+calisan2.email="a@b.com"
+print(calisan2.email)
---
+++ OOP04.py
@@ -0,0 +1,5 @@
+class calisan:
+    def __init__(self,name,surname,age):
+        self.name=name
+        self.surname=surname
+        self.age=age
