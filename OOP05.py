1fc2f94f4dc6bfdcf3bf506deaee981d20a6b40e
ozmenmustafa06 <ozmenmustafa06@gmail.com>
2024-02-05 00:45:45 +0300

    deneme

--- OOP05.py
+++ OOP05.py
@@ -1,9 +1,9 @@
 class calisan:
-    def __init__(self,name,surname,age):
+    def __init__(self,a,b,c):
         print("__init__ fonksiyonu çalışıyor.")
-        self.name=name
-        self.surname=surname
-        self.age=age
+        self.name=a
+        self.surname=b
+        self.age=c
 
 calisan1=calisan("Ali","Veli",20)
 print(calisan1.name,calisan1.surname,calisan1.age)
