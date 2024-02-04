2507d78773b1e98c5c3c535c3c90ecdc981f6dfd
ozmenmustafa06 <ozmenmustafa06@gmail.com>
2024-02-05 00:53:06 +0300

    deneme

--- OOP05.py
+++ OOP05.py
@@ -4,6 +4,13 @@ class calisan:
         self.name=a
         self.surname=b
         self.age=c
+    def show_info(self):
+        print(f"Ad:{self.name}  Soyad:{self.surname}   Yaş:{self.age}")
 
 calisan1=calisan("Ali","Veli",20)
 print(calisan1.name,calisan1.surname,calisan1.age)
+
+calisan2=calisan("Ahmet","Mehmet",25)
+print(calisan2.name,calisan2.surname,calisan2.age)
+
+calisan1.show_info()
