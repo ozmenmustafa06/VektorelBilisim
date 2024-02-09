0b524bfb6ecf38c3bf909e1669b1698174616ab6
ozmenmustafa06 <ozmenmustafa06@gmail.com>
2024-02-09 20:14:12 +0300

    deneme

---
+++ PyQt01.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import *
+aa=QApplication([])
+
+xxx=QWidget()
+xxx.show()
+
+abc=QWidget()
+abc.show()
+
+window=QWidget()
+window.show()
+aa.exec()
+
+app=QApplication([])
+
+xxx=QWidget()
+xxx.show()
+
+abc=QWidget()
+abc.show()
+
+aa.exec()
